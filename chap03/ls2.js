b80a6fb3ae26cc841c5918bac321ec72e3837ea2
Maycon Augusto <4804325+mayaugusto7@users.noreply.github.com>
2021-09-20 16:38:27 -0300

    Babel config

---
+++ chap03/ls2.js
@@ -0,0 +1,18 @@
+const fs = require('fs').promises;
+
+async function listFiles() {
+    try {
+        let dir = '.';
+        if (process.argv[2])
+            dir = process.argv[2];
+
+        const files = await fs.readdir(dir);
+        for (let fn of files) {
+            console.log(fn);
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+listFiles();
